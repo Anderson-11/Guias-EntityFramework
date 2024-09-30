7959d0137c2d96aecddab77b40b2a782602ee9f9
Anderson-11 <cq0472032023@unab.edu.sv>
2024-09-29 20:16:55 -0600

    Guía 1: EntityFramework Mostrar y Obtener Por ID Finalizada

---
+++ EFDemo/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EFDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnObtenerTodos = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnObtenerID = new System.Windows.Forms.Button();
+            this.txtObtenerID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Location = new System.Drawing.Point(12, 12);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(776, 222);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // btnObtenerTodos
+            // 
+            this.btnObtenerTodos.Location = new System.Drawing.Point(698, 255);
+            this.btnObtenerTodos.Name = "btnObtenerTodos";
+            this.btnObtenerTodos.Size = new System.Drawing.Size(90, 23);
+            this.btnObtenerTodos.TabIndex = 1;
+            this.btnObtenerTodos.Text = "Cargar Datos";
+            this.btnObtenerTodos.UseVisualStyleBackColor = true;
+            this.btnObtenerTodos.Click += new System.EventHandler(this.btnObtenerTodos_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Customer ID:";
+            // 
+            // btnObtenerID
+            // 
+            this.btnObtenerID.Location = new System.Drawing.Point(218, 252);
+            this.btnObtenerID.Name = "btnObtenerID";
+            this.btnObtenerID.Size = new System.Drawing.Size(75, 23);
+            this.btnObtenerID.TabIndex = 3;
+            this.btnObtenerID.Text = "Obtener";
+            this.btnObtenerID.UseVisualStyleBackColor = true;
+            this.btnObtenerID.Click += new System.EventHandler(this.btnObtenerID_Click);
+            // 
+            // txtObtenerID
+            // 
+            this.txtObtenerID.Location = new System.Drawing.Point(83, 252);
+            this.txtObtenerID.Name = "txtObtenerID";
+            this.txtObtenerID.Size = new System.Drawing.Size(100, 20);
+            this.txtObtenerID.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtObtenerID);
+            this.Controls.Add(this.btnObtenerID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnObtenerTodos);
+            this.Controls.Add(this.dgvCustomers);
+            this.Name = "Form1";
+            this.Text = "EntityFramework";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnObtenerTodos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnObtenerID;
+        private System.Windows.Forms.TextBox txtObtenerID;
+    }
+}
+
